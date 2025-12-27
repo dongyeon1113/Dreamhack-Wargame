704c5fb698237e7eaf191cfd819a0171766f02af
dongyeon1113 <dongyun9501@g.skku.edu>
2025-12-27 12:53:15 +0900

    Update solution.c

--- Reversing/rev-basic-5/solution.c
+++ Reversing/rev-basic-5/solution.c
@@ -2,33 +2,35 @@
     Problem: Dreamhack rev-basic-5
     Author: 강동연
     Date: 2025-12-27
-    Description: 마지막 input값이 NULL값인것을 고려한 연속적인 두 글자의 합 역연산 
-        
+    Description: 
+        문자열의 끝(NULL)을 이용한 역순(Backward) 복호화 알고리즘.
+        (input[i] + input[i+1] = data[i] 관계식을 뒤에서부터 역산)
 */
 #include <stdio.h>
+
 int main()
 {
     // 데이터 영역에 있던 비교 값들
-    unsigned char data[]={0xAD,0xD8,0xCB,0xCB,0x9D,0x97,0xCB,0xC4,0x92,0xA1,0xD2,0xD7,0xD2,0xD6,0xA8,0xA5,0xDC,0xC7,0xAD,0xA3,0xA1,0x98,0x4C};
+    unsigned char data[] = {0xAD,0xD8,0xCB,0xCB,0x9D,0x97,0xCB,0xC4,0x92,0xA1,0xD2,0xD7,0xD2,0xD6,0xA8,0xA5,0xDC,0xC7,0xAD,0xA3,0xA1,0x98,0x4C};
     unsigned char input[23];
-    int len=sizeof(data);
+    int len = sizeof(data);
     
-    
-    for (int i=len-1; i>=0; i--)
+    // 뒤에서부터 앞으로 루프 (Backward Loop)
+    for (int i = len - 1; i >= 0; i--)
     {
-        //복원한 로직 적용
-      if(i==len-1)
-      {
-        //마지막 input값이 NULL인것을 고려
-        input[i]=data[i];
-      } 
-      else{
-        //연속적인 두 글자를 더하는것의 역연산 적용
-        input[i]=data[i]-input[i+1];
+        if(i == len - 1)
+        {
+            // 마지막 글자는 NULL(0)과 더해졌으므로, data 값이 곧 input 값임
+            input[i] = data[i];
+        } 
+        else {
+            // 점화식 역산: input[i] = data[i] - input[i+1]
+            input[i] = data[i] - input[i+1];
+        }
     }
-  }
-    for (int i=0; i<len; i++)
+
+    for (int i = 0; i < len; i++)
     {
-      printf("%c",input[i]); //차례대로 출력
+        printf("%c", input[i]); // 복구된 플래그 출력
     }
 }
