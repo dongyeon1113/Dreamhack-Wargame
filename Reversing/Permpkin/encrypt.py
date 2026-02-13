ac3c82d515ca78c5b779a1a50f4d32aaf86ec46c
dongyeon1113 <dongyun9501@g.skku.edu>
2026-02-13 14:13:11 +0900

    Update encrypt.py

--- Reversing/Permpkin/encrypt.py
+++ Reversing/Permpkin/encrypt.py
@@ -1,9 +1,10 @@
 with open('rev1.txt', 'r') as f:
     content = f.read()
+with open('rev2.txt', 'r') as f:
+    content2 = f.read()
 
-# 2. 공백을 기준으로 숫자 분리 및 문자로 변환
-# chr() 함수는 10진수 아스키 값을 문자로 바꿔줍니다.
 decoded_text = "".join([chr(int(n)) for n in content.split()])
+decoded_text1 = "".join([chr(int(n)) for n in content2.split()])
 def sub_126E(a1):
     v2=0
     if a1 >=0x30 and a1 <=0x39:
@@ -21,13 +22,24 @@ def swap(x, y):
     return y, x
 def sub_11FD(a1,a2,a3,a4):
     result=0
-    for i in range(23):
-        print(a1.decode())
+    for i in range(13):
+        
         a1[0],a1[a4[i]]=a1[a4[i]],a1[0]
-        print(a1.decode())      
+            
 def sub_12E7(a1,a2):
-    for i in range(len(a1)):
-        a1[i]=a1[i]^a2[i]   
+    v4 = 13
+    v3 = len(a1)
+    if  v3 <= v4: 
+        for i in range(v3):
+            a1[i]=a1[i]^a2[i]
+    else:
+  
+        for j in range(v4):
+            a1[j] ^= a2[j]
+        for k in range(v4,v3):
+            a1[k] ^= a2[k % v4]
+    
+  
 s=bytearray(b'_this_is_sample_flag_')
 v8=bytearray(b'this_is_sample_flag')
 v7=bytearray(b'CC2A750B63821F45AC20839')
@@ -36,10 +48,15 @@ v6=bytearray(32)
 
 for i in range(23):
     v6[i]=sub_126E(v7[i]) # 여기까지는 맞음
+    print(v6[i])
+sub_11FD(s,0,len(s)-1,v6)  #sthisa_sl_fep_i_mlag_ 이게 나와야함
 
-sub_11FD(s,0,len(s)-1,v6)
-
-sub_12E7(s,v6)
+sub_12E7(s,v6) #tsbl|lWubTvoyXnUhcloY
 
 print(s)
 print(f"변환된 문자열: {decoded_text}")
+
+sub_11FD(v8,0,len(v8)-1,v6)
+sub_12E7(v8,v6)
+print(v8)
+print(f"변환된 문자열: {decoded_text1}")
